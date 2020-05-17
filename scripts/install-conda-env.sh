b83040fa9b5b887e49069ad7bcaeab66bde0b13a
jmyrberg <jesse.myrberg@gmail.com>
2020-05-17 21:10:38 +0300

    Add scripts for OSX

---
+++ scripts/install-conda-env.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Installing...
+NAME=jmyrberg-website
+conda env remove -n $NAME -y
+conda create -n $NAME python=3.7.6 -y && \  # Not GCP, but 3.7. causes problems with OS
+conda activate $NAME && \
+pip install -r ./functions/doc_context_similarity/requirements.txt \
+            -r ./functions/get_food_recommender/requirements.txt \
+            -r ./functions/post_food_recommender/requirements.txt \
+            -r ./functions/maximum_flows/requirements.txt \
+            -r ./functions/contact/requirements.txt \
+            -r ./functions/finscraper/requirements.txt \
+            -r ./functions/requirements-dev.txt && \
+echo Installation done!
---
+++ scripts/run-backend-functions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source ~/.bash_profile
+conda activate jmyrberg-website
+set -a
+source ./functions/.env.local
+set +a
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+functions-framework --source ./functions/main.py --target index --host localhost --port 8000 --debug;
